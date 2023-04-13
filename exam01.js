72a3e07767d9fb01c266802ad09a7d7b28fa59c8
yooneeee <sjy4489@gmail.com>
2023-04-13 20:07:46 +0900

    알고리즘주차TEST

---
+++ exam01.js
@@ -0,0 +1,48 @@
+function solution(num) {
+  let answer = 0;
+  let change = 1000 - num; //거스름돈
+  const coins = [500, 100, 50, 10]; //갖고 있는 동전종류(잔돈)
+
+  for (let i = 0; i < coins.length; i++) {
+    let mok = Math.floor(change / coins[i]); //잔돈 종류가 더 큰 수부터 change를 나누었을 때 몫을 구함
+    change = change - mok * coins[i]; // 거스름돈에서 해당 동전의 금액 x 몫을 뺌
+    answer += mok;
+  }
+  return answer;
+}
+let num1 = 160;
+console.log(solution(num1));
+
+/*
+let answer = 0, input = 1000 - inputs;
+
+while(input >= 500){
+    input -= 500;
+    answer++;
+}
+while(input >= 100){
+    input -= 100;
+    answer++;
+    
+}
+while(input >= 50){
+    input -= 50;
+    answer++;
+}
+while(input >= 10){
+    input -= 10;
+    answer++;
+}
+while(input >= 5){
+    input -= 5;
+    answer++;
+}
+while(input >= 1){
+    input -= 1;
+    answer++;
+}
+
+
+
+console.log(answer);
+*/
---
+++ exam02.js
@@ -0,0 +1,37 @@
+function solution(str) {
+  let sum = 0; //sum초기화
+  let count = 0; //count값 초기화
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "O") {
+      //O일 경우 count를 1씩 증가
+      count++;
+    } else {
+      //X일 경우 count를 초기화
+      count = 0;
+    }
+    sum += count;
+  }
+  return sum;
+}
+let str = "OXOOOXXXOXOOXOOOOOXO";
+console.log(solution(str));
+
+/*
+function solution(str) {
+  let sum = 0; //sum초기화
+  let count = 1; //count값 초기화
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "O") {
+      //o일 경우 count를 1씩 증가시키며 sum에 더해줌
+      sum += count;
+      count++;
+    } else {
+      //x일 경우 count를 초기화
+      count = 1;
+    }
+  }
+  return sum;
+}
+let str = "OXOOOXXXOXOOXOOOOOXO";
+console.log(solution(str));
+*/
